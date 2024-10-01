964ac25db1c608d57840f7250ffd21a944ab8b83
Chaiyapop4092 <s6503051624092@email.kmutnb.ac.th>
2024-10-01 08:24:43 +0700

    update verify

--- verify.php
+++ verify.php
@@ -11,48 +11,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>verify</title>
-    <style>
-        H1{
-            text-align: center;
-        }
-    </style>
 </head>
 <body>
-    <H1>
-        Webboard KakKak
-    </H1>
-    <hr>
-    <p align="center">
-        <?php
-            //echo "<h1 style = 'text-align: center;'>Webboard Kakkak</h1>";
+    <?php
+        //echo "<h1 style = 'text-align: center;'>Webboard Kakkak</h1>";
 
-            //$login = $_POST['login'];
-            //$pwd = $_POST['pwd'];
+        //$login = $_POST['login'];
+        //$pwd = $_POST['pwd'];
 
-            if($_POST['login'] == "admin" && $_POST['pwd'] == "ad1234"){
-                $_SESSION['username']="admin";
-                $_SESSION['role']="a";
-                $_SESSION['id']=session_id();
-                echo "ยินดีต้อนรับคุณ ADMIN <BR>";
-            }
-            elseif($_POST['login'] == "member" && $_POST['pwd'] == "mem1234"){
-                $_SESSION['username']="member";
-                $_SESSION['role']="m";
-                $_SESSION['id']=session_id();
-                echo "ยินดีต้อนรับคุณ MEMBER <BR>";
-            }
-            else{
-                echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง <BR>";
-            }
+        if($_POST['login'] == "admin" && $_POST['pwd'] == "ad1234"){
+            $_SESSION['username']="admin";
+            $_SESSION['role']="a";
+            $_SESSION['id']=session_id();
+            header("location:index.php");
+            die();
+        }
+        elseif($_POST['login'] == "member" && $_POST['pwd'] == "mem1234"){
+            $_SESSION['username']="member";
+            $_SESSION['role']="m";
+            $_SESSION['id']=session_id();
+            header("location:index.php");
+            die();
+        }
+        else{
+            $_SESSION['error']='error';
+            header("location:login.php");
+            die();
+            //echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง <BR>";
+        }
 
-            echo "<a href=index.php>
-                    กลับไปยังหน้าหลัก
-                 </a>";
+        echo "<a href=index.php>
+                กลับไปยังหน้าหลัก
+                </a>";
 
-            /*echo "เข้าสู่ระบบด้วย <BR>
-            Login = $login <BR>
-            Password = $pwd <BR>";*/
-        ?>
-    </p>
+        /*echo "เข้าสู่ระบบด้วย <BR>
+        Login = $login <BR>
+        Password = $pwd <BR>";*/
+    ?>
 </body>
 </html>
