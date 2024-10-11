0a1311ec54b5066612e1759b908377a6d12c5ac6
Chaiyapop4092 <s6503051624092@email.kmutnb.ac.th>
2024-10-11 15:07:15 +0700

    make UI and create post_save.php
    แก้หน้า post.php
    1
    • มาท า webboard kakkak กันต่ออีกซักหน่อย ท าหน้า post_save.php เพื่อเก็บข้อมูลของการตอบกลับ
    ซึ่งจะเก็บไว้ในตาราง comment โดยเราจะเก็บข้อมูลของ id ผู้ comment และ id ของกระทู้
    • อันดับแรกปรับ post.php ให้ form ส่งข้อมูลไปหา post_save.php ด้วยเมธอด POST และต้องให้ส่ง id
    ของกระทู้ไปด้วย
    
    สร้างหน้า post_save.php

--- post.php
+++ post.php
@@ -1,14 +1,15 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Post</title>
-    <style>
-        H1{
-            text-align: center;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"> 
 </head>
 <body>
     <div class="container-lg">
@@ -16,7 +17,28 @@
             Webboard KakKak
         </H1>
         <?php include "nav.php" ?>
-        
+        <div class="card text-dark bg-white border-success m-5 col-lg-8 mx-auto ">
+            <div class="card-header bg-success text-white">แสดงความคิดเห็น</div>
+            <div class="card-body">
+                <form action="post_save.php" method="post">
+                    <input type="hidden" name="post_id" value="<?=$_GET['id'];?>">
+                    <div class="row mb-3 justify-content-center">
+                        <div class="col-lg-10">
+                            <textarea name="comment" class="form-control" rows="8"></textarea>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <center>
+                                <button type="submit" class="btn btn-success btn-sm text-white">
+                                    <i class="bi bi-box-arrow-up-right-me-1"></i>ส่งข้อความ
+                                </button>
+                            </center>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </body>
 </html>
