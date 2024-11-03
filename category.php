44ef4af00a8387fc4ffff4ae89e29679c9731a84
Chaiyapop4092 <s6503051624092@gmail.com>
2024-11-03 14:17:04 +0700

    เพิ่มหน้า category.php
    • ให้แสดงชื่อหมวดหมู่จากตาราง category พร้อมมีปุ่มเพิ่มหมวดหมู่ใหม่, ปุ่มแก้ไข และปุ่มลบ
    • ถ้าคลิ๊กที่ปุ่มเพิ่มหมวดหมู่ใหม่ ให้เรียกใช้ Modal ใน Bootstrap

--- category.php
+++ category.php
@@ -15,3 +15,73 @@
     <div class="container-lg">
         <H1 style="text-align: center;" class="mt-3"> Webboard KakKak </H1>
         <?php include "nav.php" ?>
+        <table class="table table-striped mt-4">
+            <?php
+                $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+                $sql = "SELECT t3.id AS category_id, t3.name AS category_name
+                        FROM category AS t3
+                        ORDER BY t3.id";
+
+        
+                
+                
+                /*else{
+                    $sql="SELECT t3.name,t1.title,t1.id,t2.login,t1.post_date, t1.user_id FROM post as t1
+                        INNER JOIN user as t2 ON (t1.user_id=t2.id)
+                        INNER JOIN category as t3 ON (t1.cat_id=t3.id) ORDER BY t1.post_date DESC";
+                }*/
+                $result=$conn->query($sql);
+                echo "<table class='table table-striped' style='width: 60%; margin: auto; table-layout:fixed;'>
+                        <tr>
+                            <th class='text-left' style='padding-left: 15px;'>ลำดับ</th>
+                            <th class='text-alignment-center'>ชื่อหมวดหมู่</th>
+                            <th class='text-right' style='width: 9%'>จัดการ</th>
+                        </tr>";
+                while($row = $result->fetch()){
+                    echo "
+                        <tr>
+                            <td class='text-left' style='padding-left: 30px;'>$row[0]</td>
+                            <td class='text-center' style='padding-right: 255px;'>$row[1]</td>
+                            <td class='d-flex align-items-center justify-content-end'>
+                                <a href='editpost.php?id={$row[0]}' class='btn btn-warning btn-sm float-end me-2'><i class='bi bi-pencil'></i></a>
+                                <a href='delete.php?id={$row[0]}' class='btn btn-danger btn-sm float-end' onclick='return myFunction();'><i class='bi bi-trash'></i></a>
+                            </td>
+                        </tr>";
+                    /*if (isset($_SESSION['id'])){
+                        if ($_SESSION['user_id'] == $row[5]) {
+                            echo "<div>
+                                    <a href='delete.php?id={$row[2]}' class='btn btn-danger btn-sm float-end' onclick='return myFunction();'><i class='bi bi-trash'></i></a>
+                                    <a href='editpost.php?id={$row[2]}' class='btn btn-warning btn-sm float-end me-2'><i class='bi bi-pencil'></i></a>
+                                  </div>";
+                        } elseif ($_SESSION['role'] == "a") {
+                            echo "<div>
+                                    <a href='delete.php?id={$row[2]}' class='btn btn-danger btn-sm float-end' onclick='return myFunction();'><i class='bi bi-trash'></i></a>
+                                  </div>";
+                        }
+                        if($_SESSION['role'] == "a" || $_SESSION['id'] == $row['user_id']){
+                            echo "<div>
+                                    <a href='delete.php?id={$row['id']}' class='btn btn-danger btn-sm float-end' onclick='return myFunction();'><i class='bi bi-trash'></i></a>        
+                                    <a href='edit.php?id={$row['id']}' class='btn btn-warning btn-sm float-end me-2'><i class='bi bi-pencil'></i> Edit</a>
+                                </div>";
+                        }
+                    }*/
+                    //echo "</td></tr>";
+                }
+                echo "</table>";
+                echo "<center><button type='button' class='btn btn-success mt-3' data-bs-toggle='modal' data-bs-target='#addCategoryModal'>
+                        <i class='bi bi-plus-circle'></i> เพิ่มหมวดหมู่ใหม่
+                    </button></center>";
+                /*$post="SELECT post.id FROM post";
+                $result=$conn->query($post);
+                while($row=$result->fetch()){
+                    if(isset($_SESSION['id']) && $_SESSION['role']=="a"){
+                        echo "&nbsp&nbsp
+                        <a href=delete.php?id=$row[0] class='btn btn-danger btn-sm me-3' style='float: right;'><i class='bi bi-trash'></i></a>";
+                    }
+                    echo "</td></tr>";
+                }*/
+                //$conn=null;
+            ?>
+            
+        </table>
+    </div>
