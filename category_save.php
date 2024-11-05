9f1137cf6fb487404e16661da8032172478f8258
Chaiyapop4092 <s6503051624092@email.kmutnb.ac.th>
2024-11-05 14:26:09 +0700

    no duplicate detect version

--- category_save.php
+++ category_save.php
@@ -11,19 +11,19 @@ session_start();
 
     $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
 
-    $sql = "SELECT COUNT(*) FROM category WHERE name = $categoryName";
-    $result = $conn->query($sql);
-    if ($result == 0){
+    /*$sql = "SELECT COUNT(*) FROM category WHERE name = $categoryName";
+    $result = $conn->query($sql);*/
+    
         $sql = "INSERT INTO category (name) VALUES ('$categoryName')";
         
         $conn->exec($sql);
 
         $_SESSION['categoryStatus'] = "add";
         $conn = null;
-    }
-    else{
+    
+    
         $_SESSION['categoryAdd'] = "false";
-    }
+    
     header("location:category.php");
     die();
 ?>
